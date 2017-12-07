0f7e96ac90f5c7b860ebb1040494d50f6d984f00
davidchampredon <david.champredon@gmail.com>
2017-12-07 15:29:20 -0500

    improve contact tracing

--- ct-test.R
+++ ct-test.R
@@ -12,7 +12,9 @@ latent_mean <- 2
 infectious_mean <- 4
 
 # Calendar times when the GI were observed:
-t.obs <- sort(sample(x = 1:(hz/2), size = 15, replace = TRUE))
+t.obs <- sort(sample(x = 1:(hz/2), 
+                     size = 35, 
+                     replace = TRUE))
 
 G.true <- GI.seminr(latent_mean = latent_mean,
                     infectious_mean = infectious_mean, 
@@ -36,118 +38,35 @@ plot(x=1:length(G.true$incidence) * dt,
 plot(t.obs, gibck.true, typ='b',ylim=range(c(0,gibck.true,gi.obs)))
 points(x=t.obs, y=gi.obs, pch=16)
 
+# Model fixed parameters (not fitted):
+fxd.prm.seminr <- list(horizon=hz, nE=nE, nI=nI, latent_mean=latent_mean, dt=dt)
+fxd.prm.resude <- list(horizon=hz, alpha=0, kappa=0, GI_span=20, 
+                       GI_var=NULL, GI_type='pois', dt=dt)
 
-fxd.prm <- list(horizon=hz, nE=nE, nI=nI, latent_mean=latent_mean, dt=dt)
+# Fit (one for each model):
 
-nllk(R0 = 2.3, gimean = 3, t.obs = t.obs, gi.obs = gi.obs, model.epi = 'seminr', fxd.prm = fxd.prm)
+R0.rng     <- seq(1, 6, by=0.25)
+gimean.rng <- seq(2, 8, by=0.5)
+CI <- 0.95
+do.plot <- TRUE
+
+gi_ct_fit(t.obs = t.obs,
+          gi.obs = gi.obs,
+          model.epi = 'seminr',
+          fxd.prm = fxd.prm.seminr,
+          R0.rng = R0.rng,
+          gimean.rng = gimean.rng,
+          CI = CI,
+          do.plot = do.plot)
+
 
 gi_ct_fit(t.obs = t.obs, 
           gi.obs = gi.obs, 
-          model.epi = 'seminr', 
-          fxd.prm = fxd.prm,
-          R0.rng = seq(2,6,by=1), 
-          gimean.rng = seq(1,6,by=1),
-          CI = 0.95)
+          model.epi = 'resude', 
+          fxd.prm = fxd.prm.resude,
+          R0.rng = R0.rng, 
+          gimean.rng = gimean.rng,
+          CI = CI,
+          do.plot = do.plot)
 
-
-
-
-
-if(FALSE){
-    llk <- function(R0, infectmean, t.obs, gi.obs) {
-        
-        # Calculate the backward GI for given parameter values
-        # at the observed dates:
-        G <-  GI.seminr(latent_mean = latent_mean,
-                        infectious_mean = infectmean, 
-                        R0 = R0, 
-                        nE = nE, 
-                        nI = nI,
-                        cal.times.fwdbck = t.obs,
-                        horizon = hz, 
-                        dt = dt)
-        
-        # Extract the mean bckwd GI :
-        b <- G$bck.mean    # ; plot(t.obs,b,typ='b')
-        
-        # Calculate log likelihood:
-        z <- sum(dpois(x = gi.obs, lambda = b, log = TRUE))
-        return(-z)
-    }
-    
-    
-    # a <- llk(R0=3, infectmean=4, t.obs, gi.obs)
-    
-    R0.rng <- seq(1.0, 6, by=0.5)
-    gimean.rng <- seq(1,10,by=1)
-    
-    system.time(
-        M <- outer(X = R0.rng, 
-                   Y = gimean.rng, 
-                   FUN = Vectorize(llk, list("R0","infectmean")), 
-                   t.obs=t.obs, 
-                   gi.obs=gi.obs)
-    )
-    
-    idx <- which(M == min(M, na.rm = TRUE), 
-                 arr.ind = TRUE)
-    
-    # Retrieve neg log likelihood min
-    ll.min <- M[idx]
-    # Set confidence threshold
-    confidence.threshold = 0.90
-    # Calculate the level on the likelihoof function
-    conf.cutoff <- ll.min+ qchisq(confidence.threshold,2)/2
-    
-    
-    contour(x = R0.rng, 
-            y = gimean.rng, 
-            z = M, 
-            nlevels = 30,
-            col='grey',
-            xlab = 'R0', ylab = 'Mean GI')
-    points(x = R0.true, y=infectious_mean, 
-           cex=2, pch=3, col='blue', lwd=3)
-    points(x = R0.rng[idx[1]], y = gimean.rng[idx[2]], pch=1, cex=3,lwd=3, col='red')
-    # points(x = naive.fit['R0'], y = naive.fit['infectmean'], pch=4, cex=2)
-    # Draw the confidence contour
-    contour(R0.rng,
-            gimean.rng,
-            M,
-            level = conf.cutoff,
-            labels="",
-            col="red",
-            lwd=3, 
-            lty=1,
-            add = TRUE)
-    
-    # Retrieve the extreme values of the confidence contour
-    cc = contourLines(R0.rng,gimean.rng,M,level = conf.cutoff)#[[1]]
-    
-    
-    
-    # Finally, let’s visually check how good the fit is:
-    
-    tu <- unique(t.obs)
-    Gfit <- GI.resude(cal.times.fwdbck = tu,
-                      R0 = fitted.prm['R0'],
-                      alpha = 0,
-                      kappa = 0,
-                      GI_span = 20,
-                      GI_mean = fitted.prm['gimean'],
-                      GI_var = NA,
-                      GI_type = 'pois',
-                      horizon = 200)
-    
-    gbck.fit <- Gfit$bck.mean
-    
-    plot(x = tu, y = gbck.fit, 
-         ylim = range(gbck.fit,gi.obs),
-         typ='o', pch=16,lwd=3,
-         las = 1, xlab = 'calendar time', ylab='days',
-         main = 'RESuDe model fitted to contact tracing data')
-    points(t.obs, gi.obs, pch=3, col='red',lwd=6,cex=2)
-    grid()
-    legend('topleft', legend = c('data','model fit'),col=c('red','black'),pch=c(3,16),lwd=c(NA,3),pt.cex = c(1,1), pt.lwd = c(6,1))
-    
-}
+message('|=== CT TEST DONE ===|')
--- ct.R
+++ ct.R
@@ -46,16 +46,12 @@ nllk <- function(R0,
     }
     # Extract the mean bckwd GI :
     b <- G$bck.mean    
+    b[b==0] <- 1e-6
     
     # Calculate log likelihood:
     z <- sum(dpois(x = gi.obs, lambda = b, log = TRUE))
     return(-z)
 }
-
-
-
-
-
 
 
 #' Fit R0 and mean generation interval from contact tracing data
@@ -64,6 +60,10 @@ nllk <- function(R0,
 #' @param gi.obs Numeric vector. Backward generation intervals observed.
 #' @param model.epi String. Epidemic model used. Choice between \code{seminr} or \code{resude}.
 #' @param fxd.prm List. Parameters of \code{model.epi} that are fixed.
+#' @param R0.rng Numeric vector. Values of R0 explored to calculate the likelihood surface.
+#' @param gimean.rng Numeric vector. Values of mean intrinsic GI explored to calculate the likelihood surface.
+#' @param CI Numeric. Confidence interval level. Default = 0.95.
+#' @param do.plot Boolean. Fitting diagnostic plots. Default = TRUE.
 #' @export
 gi_ct_fit <- function(t.obs, 
                       gi.obs, 
@@ -71,7 +71,10 @@ gi_ct_fit <- function(t.obs,
                       fxd.prm,
                       R0.rng, 
                       gimean.rng,
-                      CI = 0.95) {
+                      CI = 0.95,
+                      do.plot = FALSE ) {
+    
+    t1 <- as.numeric(Sys.time())
     
     M <- outer(X = R0.rng, 
                Y = gimean.rng, 
@@ -86,33 +89,165 @@ gi_ct_fit <- function(t.obs,
     
     # Retrieve neg log likelihood min
     ll.min <- M[idx]
-    # Set confidence threshold
-    CI = 0.90
+    
     # Calculate the level on the likelihoof function
     conf.cutoff <- ll.min + qchisq(CI,2)/2
     
+    # Estimate the confidence interval from the contour lines
+    cc <- contourLines(R0.rng,
+                       gimean.rng,
+                       M,
+                       level = conf.cutoff)
+    x.tmp <- list()
+    y.tmp <- list()
+    
+    for(i in seq_along(cc)){
+        x.tmp[[i]] <- range(cc[[i]]$x)
+        y.tmp[[i]] <- range(cc[[i]]$y)
+    }
+    R0.ci     <- range(unlist(x.tmp))
+    gimean.ci <- range(unlist(y.tmp))
+    
     # Likelihood surface:
-    contour(x = R0.rng, 
-            y = gimean.rng, 
-            z = M, 
-            nlevels = 30,
-            col='grey',
-            xlab = 'R0', ylab = 'Mean GI')
-    
-    # Best point estimate:
-    points(x = R0.rng[idx[1]], 
-           y = gimean.rng[idx[2]],
-           pch=1, cex=3,lwd=3, col='red')
-    
-    # Draw the confidence contour
-    contour(R0.rng,
-            gimean.rng,
-            M,
-            level = conf.cutoff,
-            labels="",
-            col="red",
-            lwd=3, 
-            lty=1,
-            add = TRUE)
+    if(do.plot){
+        title <- paste0('Neg. Log-Likelihood Surface (',model.epi,')\n with ',CI*100,'%CI')
+        ylab <- 'Mean intrinsic GI'
+        if(model.epi=='seminr') ylab <- 'Mean infectious period'
+        
+        contour(x = R0.rng, 
+                y = gimean.rng, 
+                z = M, 
+                nlevels = 20,
+                col='grey',
+                main = title,
+                xlab = 'R0', 
+                ylab = ylab,
+                las = 1)
+        
+        # Best point estimate:
+        col.best <- 'red'
+        R0.best     <- R0.rng[idx[1]]
+        gimean.best <- gimean.rng[idx[2]]
+        
+        points(x = R0.best, 
+               y = gimean.best,
+               pch=16, cex=3, col=col.best)
+        segments(x0 = R0.best, 
+                 y0 = 0,
+                 x1 = R0.best,
+                 y1 = gimean.best,
+                 col = col.best, lty=2)
+        segments(x0 = 0,
+                 y0 = gimean.best,
+                 x1 = R0.best,
+                 y1 = gimean.best,
+                 col = col.best, lty=2)
+        
+        # Draw the confidence contour
+        contour(R0.rng,
+                gimean.rng,
+                M,
+                level = conf.cutoff,
+                labels="",
+                col="red",
+                lwd=3, 
+                lty=1,
+                add = TRUE)
+        
+        # Plot the fitted GI means:
+        
+        if(model.epi=='seminr'){
+            im <- 2*(gimean.best - fxd.prm[['latent_mean']])  # gi.mean ~ latent + infectious/2
+            im.lo <- 2*(gimean.ci[1] - fxd.prm[['latent_mean']])  # gi.mean ~ latent + infectious/2
+            im.hi <- 2*(gimean.ci[2] - fxd.prm[['latent_mean']])  # gi.mean ~ latent + infectious/2
+            if(im <= 0) im <- 1
+            if(im.lo <= 0) im.lo <- 1
+            if(im.hi <= 0) im.hi <- 1
+            Gfit <- GI.seminr(latent_mean = fxd.prm[['latent_mean']],
+                              infectious_mean = im, 
+                              R0 = R0.best, 
+                              nE = fxd.prm[['nE']], 
+                              nI = fxd.prm[['nI']],
+                              cal.times.fwdbck = t.obs,
+                              horizon = fxd.prm[['horizon']], 
+                              dt = fxd.prm[['dt']])
+            
+            Gfit.lo <- GI.seminr(latent_mean = fxd.prm[['latent_mean']],
+                              infectious_mean = im.lo, 
+                              R0 = R0.ci[1], 
+                              nE = fxd.prm[['nE']], 
+                              nI = fxd.prm[['nI']],
+                              cal.times.fwdbck = t.obs,
+                              horizon = fxd.prm[['horizon']], 
+                              dt = fxd.prm[['dt']])
+            Gfit.hi <- GI.seminr(latent_mean = fxd.prm[['latent_mean']],
+                                 infectious_mean = im.hi, 
+                                 R0 = R0.ci[2], 
+                                 nE = fxd.prm[['nE']], 
+                                 nI = fxd.prm[['nI']],
+                                 cal.times.fwdbck = t.obs,
+                                 horizon = fxd.prm[['horizon']], 
+                                 dt = fxd.prm[['dt']])
+            
+        }
+        if(model.epi=='resude'){
+            Gfit <- GI.resude(cal.times.fwdbck = t.obs,
+                              R0 = R0.best,
+                              alpha = fxd.prm[['alpha']], 
+                              kappa = fxd.prm[['kappa']], 
+                              GI_span = fxd.prm[['GI_span']], 
+                              GI_mean = gimean.best, 
+                              GI_var = fxd.prm[['GI_var']], 
+                              GI_type = fxd.prm[['GI_type']],
+                              horizon = fxd.prm[['horizon']])
+            Gfit.lo <- GI.resude(cal.times.fwdbck = t.obs,
+                              R0 = R0.ci[1],
+                              alpha = fxd.prm[['alpha']], 
+                              kappa = fxd.prm[['kappa']], 
+                              GI_span = fxd.prm[['GI_span']], 
+                              GI_mean = gimean.ci[1], 
+                              GI_var = fxd.prm[['GI_var']], 
+                              GI_type = fxd.prm[['GI_type']],
+                              horizon = fxd.prm[['horizon']])
+            Gfit.hi <- GI.resude(cal.times.fwdbck = t.obs,
+                                 R0 = R0.ci[2],
+                                 alpha = fxd.prm[['alpha']], 
+                                 kappa = fxd.prm[['kappa']], 
+                                 GI_span = fxd.prm[['GI_span']], 
+                                 GI_mean = gimean.ci[2], 
+                                 GI_var = fxd.prm[['GI_var']], 
+                                 GI_type = fxd.prm[['GI_type']],
+                                 horizon = fxd.prm[['horizon']])
+        }
+        
+        gbck.fit <- Gfit$bck.mean
+        gbck.fit.lo <- Gfit.lo$bck.mean
+        gbck.fit.hi <- Gfit.hi$bck.mean
+        
+        plot(x = t.obs, 
+             y = gbck.fit, 
+             ylim = range(gbck.fit,gi.obs),
+             typ='o', pch=16, lwd=3,
+             col = col.best,
+             las = 1, 
+             xlab = 'calendar time',
+             ylab='Mean Backward GI',
+             main = paste(model.epi, 'model backward GI\nfitted to contact tracing data'))
+        lines(x=t.obs, y=gbck.fit.lo, lty=2, col=col.best, lwd=2)
+        lines(x=t.obs, y=gbck.fit.hi, lty=2, col=col.best, lwd=2)
+        points(t.obs, gi.obs, pch=1, col='black',lwd=1.5,cex=1)
+        grid()
+        legend('topleft', legend = c('data','model fit',paste(CI*100,'%CI')),
+               col=c('black',col.best,col.best),pch=c(1,16,NA),lwd=c(NA,3,2),
+               pt.cex = c(1,1,NA), pt.lwd = c(1.5,1,NA), lty=c(1,1,2))
+    }
+    t2 <- as.numeric(Sys.time())
+    dt <- round( (t2-t1)/60, 1)
+    msg <- paste('Fit to contact tracing data done in',dt,'minute(s).')
+    message(msg)
+    return(list(R0.best = R0.best,
+                gimean.best = gimean.best,
+                R0.ci = R0.ci,
+                gimean.ci = gimean.ci))
     
 }
