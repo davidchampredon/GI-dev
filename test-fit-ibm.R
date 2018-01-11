613628775aa29f16ee101f4bb4798b1685a50af9
davidchampredon <david.champredon@gmail.com>
2018-01-11 15:17:36 -0500

    add (temporary?) test file

--- test-fit-ibm.R
+++ test-fit-ibm.R
@@ -1,20 +1,29 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2) ; theme_set(theme_bw())
-
 library(devtools) 
-install_github("davidchampredon/seminribm", build_vignettes = FALSE, force=TRUE)
-install_github("davidchampredon/GI", build_vignettes = FALSE, force=TRUE)
 
-library(seminribm)
+# Load (and download if needed) my libraries
+lib.GI <- try(library(GI))
+if(class(lib.GI)=='try-error'){
+install_github("davidchampredon/GI", 
+               build_vignettes = FALSE, force=TRUE)
 library(GI)
+}
+
+lib.seminribm <- try(library(seminribm))
+if(class(lib.seminribm)=='try-error'){
+    install_github("davidchampredon/seminribm", 
+                   build_vignettes = FALSE, force=TRUE)
+    library(seminribm)
+}
 
 set.seed(1234)
 
 # ---- Generate data from an individual-based model ----
 
 horizon <- 100
-popSize <- 2e3
+popSize <- 2e3  # warning: above 5e3 takes long time!
 
 initInfectious  <- 2
 R0              <- 3.0
@@ -27,6 +36,7 @@ doExact         <- FALSE
 timeStepTauLeap <- 0.1
 rnd_seed        <- 1234
 
+# See: ?seminribm_run
 sim <- seminribm_run(horizon,
                      popSize ,
                      R0 ,
@@ -40,10 +50,9 @@ sim <- seminribm_run(horizon,
                      rnd_seed ,
                      calc_WIW_Re)
 
-# Retrieve backward generation intervals:
+# Retrieve backward generation intervals from simulation:
 b  <- sim$GI_bck
 at <- sim$acq_times
-
 
 # ---- Sampled imperfectly observed GIs ----
 
@@ -51,17 +60,20 @@ at <- sim$acq_times
 # - not all GIs are observed
 # - there is an observation error
 
+# Sample the GIs observed:
 prop.observed <- 0.1  # proportion of GIs observed
 n.obs <- round(prop.observed*popSize)  # number of bckwd GIs observed
 idx.obs <- sample(x = 1:length(b), size = n.obs, replace = FALSE)
-
 gi.obs.true <- b[idx.obs]
 at.obs <- at[idx.obs]
 
 # Add observation error:
-gi.obs <- rnorm(n = n.obs, mean = gi.obs.true, sd = 0.5)
+sd.err <- 0.5
+gi.obs <- rnorm(n = n.obs, mean = gi.obs.true, sd = sd.err)
 gi.obs[gi.obs<1] <- 1
 gi.obs <- round(gi.obs)
+
+# Visualize 'true' vs observed:
 plot(gi.obs.true, gi.obs, las=1,
      main = 'observation error for backward GI')
 grid()
@@ -86,36 +98,22 @@ df.b %>%
     xlab('calendar time')+ylab('days')
 
 
-# ---- Fit model from GIs ----
+# ---- Fit ReSuDe model from GIs ----
 
-fxd.prm.seminr <- list(horizon=horizon, 
-                       nE=nE, 
-                       nI=nI, 
-                       latent_mean=latent_mean, 
+fxd.prm.resude <- list(horizon=horizon, 
+                       alpha=0, 
+                       kappa=0, 
+                       GI_span = 20, 
+                       GI_var = NULL, 
+                       GI_type = 'pois', 
                        dt = 1.0)
-
-fxd.prm.resude <- list(horizon=horizon, alpha=0, kappa=0, 
-                       GI_span=20, 
-                       GI_var=NULL, 
-                       GI_type='pois', 
-                       dt=1.0)
 
 R0.rng     <- seq(1, 6, by=0.5)
 gimean.rng <- seq(2, 8, by=0.5)
 CI <- 0.95
 do.plot <- TRUE
 
-if(FALSE){ # Takes too much time... 
-    fit.seminr <- gi_ct_fit(t.obs = at.obs,
-                            gi.obs = gi.obs,
-                            model.epi = 'seminr',
-                            fxd.prm = fxd.prm.seminr,
-                            R0.rng = R0.rng,
-                            gimean.rng = gimean.rng,
-                            CI = CI,
-                            do.plot = do.plot)
-}
-
+# See: ?gi_ct_fit
 fit.resude <- gi_ct_fit(t.obs = at.obs, 
                         gi.obs = gi.obs, 
                         model.epi = 'resude', 
